7a9e73b54456986fd8c64e0927443681da0765a9
ghlyibu <1091291816@qq.com>
2022-09-12 17:47:12 +0800

    feat: 实现effect返回

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,33 +1,33 @@
 class ReactiveEffect {
-    private _fn:any
+    private _fn: () => any;
 
     constructor(fn) {
-        this._fn = fn
+        this._fn = fn;
     }
     run() {
-        activeEffect = this
-        this._fn()
+        activeEffect = this;
+        return this._fn();
     }
 }
 const targetMap = new Map();
 export function track(target, key) {
     let depsMap = targetMap.get(target);
-    if(!depsMap){
+    if (!depsMap) {
         depsMap = new Map();
-        targetMap.set(target, depsMap)
+        targetMap.set(target, depsMap);
     }
-    let dep = depsMap.get(key)
-    if(!dep){
-        dep = new Set()
-        depsMap.set(key, dep)
+    let dep = depsMap.get(key);
+    if (!dep) {
+        dep = new Set();
+        depsMap.set(key, dep);
     }
-    dep.add(activeEffect)
+    dep.add(activeEffect);
 }
-export function trigger (target, key) {
+export function trigger(target, key) {
     let depsMap = targetMap.get(target);
     let dep = depsMap.get(key);
-    for(const effect of dep){
-        effect.run()
+    for (const effect of dep) {
+        effect.run();
     }
 }
 
@@ -36,4 +36,5 @@ export function effect(fn) {
     // fn
     const _effect = new ReactiveEffect(fn);
     _effect.run();
+    return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,23 +1,37 @@
 import { effect } from "../effect";
 import { reactive } from "../reactive";
 
-describe('effect',() => {
+describe('effect', () => {
 
     it("happy path", () => {
         const user = reactive({
             age: 10,
-        })
-        
+        });
+
         let nextAge;
-        effect(()=>{
+        effect(() => {
             nextAge = user.age + 1;
             console.log(nextAge);
         });
 
-        expect(nextAge).toBe(11)
+        expect(nextAge).toBe(11);
 
         // update
         user.age++;
-        expect(nextAge).toBe(12)
-    })
-})
+        expect(nextAge).toBe(12);
+    });
+
+    it("should return runner when call effect", () => {
+        // 1. effect(fn) -> function (runner) -> fn -> return
+        let foo = 10;
+        const runner = effect(() => {
+            foo++;
+            return "foo";
+        });
+
+        expect(foo).toBe(11);
+        const r = runner();
+        expect(foo).toBe(12);
+        expect(r).toBe("foo");
+    });
+});
