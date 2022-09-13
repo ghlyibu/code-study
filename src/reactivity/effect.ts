055ad663fdddbc65ffd70e73748cbedbe2056dfb
ghlyibu <1091291816@qq.com>
2022-09-13 20:37:59 +0800

    feat: readonly和effect 的 stop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,32 @@
 class ReactiveEffect {
-    private _fn: () => any;
+    deps: [] = [];
+    active = true
+    onStop?: () => void;
     public scheduler?: () => any;
-    constructor(fn, scheduler?: () => any) {
-        this._fn = fn;
+    constructor(public fn: () => any, scheduler?: () => any) {
+        this.fn = fn;
         this.scheduler = scheduler;
     }
     run() {
         activeEffect = this;
-        return this._fn();
+        return this.fn();
+    }
+    stop() {
+        if (this.active) {
+            cleanupEffect(this);
+            if (this.onStop) {
+                this.onStop();
+            }
+            this.active = false;
+        }
     }
 }
+function cleanupEffect(effect: ReactiveEffect) {
+    effect.deps.forEach((dep: Set<ReactiveEffect>) => {
+        dep.delete(effect);
+    });
+}
+
 const targetMap = new Map();
 export function track(target, key) {
     let depsMap = targetMap.get(target);
@@ -22,7 +39,10 @@ export function track(target, key) {
         dep = new Set();
         depsMap.set(key, dep);
     }
-    dep.add(activeEffect);
+    if (activeEffect) {
+        dep.add(activeEffect);
+        activeEffect.deps.push(dep);
+    }
 }
 export function trigger(target, key) {
     let depsMap = targetMap.get(target);
@@ -41,6 +61,15 @@ export function effect(fn, options: Record<string, any> = {}) {
     // fn
     const scheduler = options.scheduler;
     const _effect = new ReactiveEffect(fn, scheduler);
+    _effect.onStop = options.onStop
+
     _effect.run();
-    return _effect.run.bind(_effect);
+    
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
+
+export function stop(runner) {
+    runner.effect.stop();
 }
---
+++ src/reactivity/tests/readonly.spec.ts
@@ -0,0 +1,19 @@
+import { readonly } from "../reactive";
+
+describe('reactive', () => {
+    it('happy path', () => {
+        const original = { foo: 1, bar: { baz: 2 } };
+        const observed = readonly(original);
+        expect(observed).not.toBe(original);
+        expect(observed.foo).toBe(1);
+    });
+
+    it("wran then call set", () => {
+        console.warn = jest.fn();
+        const user = readonly({
+            age: 10
+        });
+        user.age = 11;
+        expect(console.warn).toBeCalled()
+    });
+});
