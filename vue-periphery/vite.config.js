4a68937bc3ef54341c5e6d1eaaed8b86ecbd3e61
ghlyibu <1091291816@qq.com>
2022-11-15 21:37:19 +0800

    vue-router原理学习

--- vue-periphery/vite.config.js
+++ vue-periphery/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
+function _resolve(dir) {
+  return path.resolve(__dirname, dir)
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(),vueJsx()],
+  // 配置项目别名
+  resolve: {
+    alias: {
+      '@': _resolve('src'),
+    },
+  },
 })
