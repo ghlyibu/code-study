4f26de78bf2fec799a861cf2164fe33b39bb9ad6
ghlyibu <1091291816@qq.com>
2023-03-22 07:11:13 +0800

    vue,ts文件支持

--- webpack/webpack-vue3/config/webpack.dev.js
+++ webpack/webpack-vue3/config/webpack.dev.js
@@ -1,23 +1,50 @@
 const path = require("path");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {VueLoaderPlugin} = require("vue-loader");
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, '../src/main.js'),
+    entry: path.resolve(__dirname, '../src/main.ts'),
     output: {
         path: path.resolve(__dirname, "../dist"),
         filename: "js/[name].js"
     },
     resolve: {
         alias: {
-            "@":path.resolve("./src")
-        }
+            "@": path.resolve(__dirname,"../src")
+        },
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".vue", ".json"]
     },
     module: {
-        // loader
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.tsx?$/, // ts或tsx
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                            happyPackMode: false,
+                            appendTsSuffixTo: ["\\.vue$"] // 用于编译 .vue文件中的ts
+                        }
+                    }
+                ],
+
+            }
+        ]
     },
     plugins: [
+        new VueLoaderPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "../public/index.html")
@@ -26,7 +53,7 @@ module.exports = {
     devServer: {
         port: 8080,
         static: {
-           directory: path.join(__dirname, "../public")
+            directory: path.join(__dirname, "../public")
         }
     }
 }
---
+++ webpack/webpack-vue3/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{ count }}
+        <button @click="addCount">add</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { reactive, toRefs } from 'vue';
+
+export default {
+    setup() {
+        const state = reactive({
+            count: 0,
+        });
+        const addCount = () => {
+            state.count++;
+        };
+
+        return {
+            ...toRefs(state),
+            addCount
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
